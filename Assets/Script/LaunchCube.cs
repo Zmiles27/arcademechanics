09257d7cf1aeb72f05a3423d8517364b734abae0
unknown <34804@ma-web.nl>
2023-02-14 11:56:07 +0100

    got in some colours

--- Assets/Script/LaunchCube.cs
+++ Assets/Script/LaunchCube.cs
@@ -17,7 +17,6 @@ public class LaunchCube : MonoBehaviour
     {
         if (Input.GetButton("Jump"))
         {
-            //Apply a force to this Rigidbody in direction of this GameObjects up axis
             m_Rigidbody.AddForce(transform.up * m_Thrust);
         }
     }
